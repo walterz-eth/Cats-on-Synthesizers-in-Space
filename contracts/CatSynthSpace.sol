ff9eabbc67be718dfaca6a2d71388287b1eecdb1
walterz <tanisssss@protonmail.com>
2022-10-27 00:16:52 -0300

    Using 'console.log' for ganache to easy debugging

--- contracts/CatSynthSpace.sol
+++ contracts/CatSynthSpace.sol
@@ -6,6 +6,9 @@ import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
+import "@ganache/console.log/console.sol";
+//import "hardhat/console.sol"; // for hardhat workspaces
+
 contract CatSynthSpace is ERC721, ERC721URIStorage, Ownable {
     using Counters for Counters.Counter;
 
@@ -37,5 +40,22 @@ contract CatSynthSpace is ERC721, ERC721URIStorage, Ownable {
         returns (string memory)
     {
         return super.tokenURI(tokenId);
+    }
+
+    /**
+     * @notice Used to buy the next available token
+     * @dev We will simulate price fluctuations by setting price related to current tokenId available.
+     */
+    function buyToken () external payable {        
+        uint256 tokenId = _tokenIdCounter.current();
+
+        // We have to log while transaction is running, so not possible to log anything AFTER the require statements.
+        // REMEMBER this logs will appear on ganache console/log files, not truffle's!
+        console.log (tokenId, msg.value);
+
+        require (msg.value == tokenId * 0.1 ether, "Not enough ETH to buy a token");
+
+        _tokenIdCounter.increment(); // for later use
+        _safeMint (msg.sender, tokenId);
     }
 }
--- test/CatSynthSpace.test.js
+++ test/CatSynthSpace.test.js
@@ -2,7 +2,7 @@ const CatSynthSpace = artifacts.require ("CatSynthSpace");
 const truffleAssert = require ("truffle-assertions");
 
 contract ("CatSynthSpace",(accounts) => {
-    it ("Should mint a token and send to expected account", async() => {
+    it("Should mint a token and send to expected account", async () => {
         const instance = await CatSynthSpace.deployed();
         let txResult = await instance.safeMint (accounts[1], "0");
         
@@ -15,6 +15,9 @@ contract ("CatSynthSpace",(accounts) => {
 
         assert.equal (await instance.ownerOf(0), accounts[1], "Owner of token is not the expected");
 
-        console.log (await instance.tokenURI(web3.utils.toBN(0)));
+        //console.log(await instance.tokenURI(web3.utils.toBN(0)));
+        
+        const tx = await instance.buyToken({ sender: accounts[1], value: web3.utils.toWei("0.1", "ether") });
+        
     })
 });
